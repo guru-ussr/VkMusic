1e885c47ab3ceb77c30aba135dbc63bd13907bc4
guru-ussr <kxalatov@mail.ru>
2019-11-09 11:23:59 +0300

    v.1.0.0

---
+++ VkMusicForm1/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace VkMusicForm1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupAuth = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupAuth.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 42);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(539, 368);
+            this.checkedListBox1.TabIndex = 14;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.button3.Location = new System.Drawing.Point(557, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 98);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Download";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label5.Location = new System.Drawing.Point(12, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 25);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Treck List";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 416);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(539, 20);
+            this.progressBar1.TabIndex = 21;
+            // 
+            // groupAuth
+            // 
+            this.groupAuth.Controls.Add(this.label4);
+            this.groupAuth.Controls.Add(this.textBox2);
+            this.groupAuth.Controls.Add(this.label3);
+            this.groupAuth.Controls.Add(this.label2);
+            this.groupAuth.Controls.Add(this.textBox1);
+            this.groupAuth.Controls.Add(this.button1);
+            this.groupAuth.Controls.Add(this.label1);
+            this.groupAuth.Location = new System.Drawing.Point(1, -6);
+            this.groupAuth.Name = "groupAuth";
+            this.groupAuth.Size = new System.Drawing.Size(729, 464);
+            this.groupAuth.TabIndex = 22;
+            this.groupAuth.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label4.ForeColor = System.Drawing.Color.Crimson;
+            this.label4.Location = new System.Drawing.Point(254, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(212, 20);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Incorrect login or password\r\n";
+            this.label4.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.textBox2.Location = new System.Drawing.Point(247, 237);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(239, 31);
+            this.textBox2.TabIndex = 27;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label3.Location = new System.Drawing.Point(243, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Passwd";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label2.Location = new System.Drawing.Point(243, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Login";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.textBox1.Location = new System.Drawing.Point(247, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 31);
+            this.textBox1.TabIndex = 24;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.button1.Location = new System.Drawing.Point(247, 294);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 41);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Log In";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.label1.Location = new System.Drawing.Point(250, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 39);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Authorization";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 444);
+            this.Controls.Add(this.groupAuth);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "--";
+            this.groupAuth.ResumeLayout(false);
+            this.groupAuth.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupAuth;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ VkMusicForm1/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using VkNet;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+using Microsoft.Extensions.DependencyInjection;
+using VkNet.AudioBypassService.Extensions;
+using System.Net;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace VkMusicForm1
+{
+    public partial class Form1 : Form
+    {
+        
+        static public VkApi api;
+        static public VkNet.Utils.VkCollection<VkNet.Model.Attachments.Audio> audios;
+        static public List<string> urlList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Кнопка LogIn
+        public void button1_Click(object sender, EventArgs e)
+        {
+            ServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddAudioBypass();
+            api = new VkApi(serviceCollection);
+            bool auth = false;
+            
+            // Авторизация
+            try // Обработка неправильного логина или пароля
+            {
+                api.Authorize(new ApiAuthParams
+                {
+                    Login = textBox1.Text,
+                    Password = textBox2.Text,
+                    TwoFactorAuthorization = () =>
+                    {
+                        return api.Token;
+                    }
+                });
+
+                auth = true;
+                label4.Visible = false;
+                groupAuth.Visible = false;
+            }
+            catch (VkNet.AudioBypassService.Exceptions.VkAuthException)
+            {
+                label4.Visible = true;
+            }
+            if (auth != true) {
+                return;
+            }
+            var res = api.Groups.Get(new GroupsGetParams());
+            GetAudio();
+        }
+
+        // Получаем список аудиозаписей пользователя 
+        private void GetAudio()
+        {
+            audios = api.Audio.Get(new AudioGetParams { Count = 100 });
+            int n = 0;
+            foreach (var audio in audios)
+            {
+                checkedListBox1.Items.Add($" {n} > {audio.Artist} - {audio.Title}");
+                n += 1;
+            }
+        }
+
+        // Кнопка Download, скачиваем выбранную музыку по номерам
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int count = checkedListBox1.CheckedItems.Count;
+            
+            for (int j = 0; j < count; j++)
+            {
+
+                int v = Convert.ToInt32(checkedListBox1.CheckedItems[j].ToString()[1].ToString());
+                string url = UrlDec(audios[v].Url.ToString());
+
+                //Thread myThread = new Thread(Download);
+                Download(url, v);
+                
+            }
+        }
+
+        public void Download(string url, int v)
+        {
+            WebClient webClient = new WebClient();
+
+            webClient.DownloadProgressChanged += (o, args) => progressBar1.Value = args.ProgressPercentage;
+            webClient.DownloadFileCompleted += (o, args) => progressBar1.Value = 100;
+            webClient.DownloadFileAsync(new Uri(url), $@"C:\Users\user\Downloads\{ audios[v].Artist}
+                - { audios[v].Title}.mp3");
+            webClient.DownloadFile(url, $@"C:\Users\user\Downloads\{audios[v].Artist} - {audios[v].Title}.mp3");
+            progressBar1.Value = 0;
+        }
+
+        // Парсим url котрый получили и создаём рабочую ссылку 
+        public string UrlDec(string vkurl)
+        {
+            string url;
+            string temp = vkurl.Replace("/index.m3u8", ".mp3");
+
+            Regex regex = new Regex(@"/p\w*/");
+            Match match = regex.Match(temp);
+            string simvol = match.Value;
+
+            regex = new Regex(@"/p\w*/\w*/");
+            url = regex.Replace(temp, simvol);
+
+            return url;
+        }
+    }
+}
